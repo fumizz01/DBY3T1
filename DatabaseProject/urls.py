24a6d1d4194afa2c99c1a0085554c4b73b632d34
pnmine <pathza.2305@gmail.com>
2023-11-12 18:09:27 +0700

    add link

--- DatabaseProject/urls.py
+++ DatabaseProject/urls.py
@@ -20,4 +20,6 @@ from project import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='Index'),
+    path('register.html', views.register, name='register'),
+    
 ]
