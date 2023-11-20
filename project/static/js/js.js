7ab18f6d9a4fdb72857197aa8b2bd11e2f85ed12
pnmine <pathza.2305@gmail.com>
2023-11-20 18:15:55 +0700

    New Validate register form add some exam ajax and request.POST

--- project/static/js/js.js
+++ project/static/js/js.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
         }
       });
 
+    /* show password strength */
     $('#password-strength-status').hide();
     $('.pswd_info').hide();
     $('#register-password').keyup(function(event) {
@@ -17,22 +18,62 @@ $(document).ready(function () {
         checkPasswordStrength(password);
       });
         
-    $('#register-password, #password_confirm').on('keyup', function () {
+/*     $('#register-password, #password_confirm').on('keyup', function () {
         if ($('#register-password').val() == $('#password_confirm').val()) {
             $('#message').html('Matching').css('color', 'green');
             document.getElementById('register-button').disabled = false;
         } else {
-            $('#message').html('Not Matching').css('color', 'red');
-            document.getElementById('register-button').disabled = true;
-
-            
-
-        }
-    });
+            if  ($('#register-password').val() != ''){
+                $('#message').html('Not Matching').css('color', 'red');
+            }
+            console.log($('#message').html())
+            var para = document.createElement("p");
+            if ($('#message').html() == 'Not Matching'){
+                    $("#register-button").click(function()
+                {
+                    $('#register-button').css('border', '1px solid red');
+                    para.innerHTML = "You need to correctly confirm password";
+                    para.style.cssText ='position:absolute; text-align:center; color:red; margin-top:35px; margin-left: 3%; display: inline-block;';
+                    document.getElementById('register-button').disabled = true;
+                });
+            };
     
+        };
+        document.getElementById('message').appendChild(para);
+    }); */
 
+    /* $('#register-form').submit(function(e) {
+        e.preventDefault();
+        validateInputs();
+        return true
+    }); */
+    $( "#register-form" ).on( "submit", function(event) {
+        event.preventDefault();
+        if (validateInputs()) {
+            // If all validations pass, send a POST request
+            const formData = $("#register-form").serialize(); // Serialize the form data
+    
+            $.ajax({
+                type: 'POST',
+                url: '', // Replace with your server endpoint URL
+                data: formData,
+                success: function (response) {
+                    // Handle the success response from the server
+                    console.log(response);
+                    alert('Form submitted successfully! Redirecting...');
+                    window.location.href = '/login'; // Replace with the URL you want to redirect to
+                },
+                error: function (error) {
+                    // Handle errors
+                    console.error('Error submitting form:', error);
+                    alert('Error submitting form. Please try again.');
+                }
+            });
+        }
+      });
 
 });
+
 
 
 /* password */
@@ -49,7 +90,7 @@ function checkPasswordStrength(password) {
 	var numbers        = password.match(number) ? 1 : 0;
 	var special        = password.match(specialCharacters) ? 1 : 0;
 
-	this.update_info('length', password.length >= 6 && password.length <= 15);
+	this.update_info('length', password.length >= 8 && password.length <= 15);
     this.update_info('capital', capitalletters);
     this.update_info('small', loweletters);
     this.update_info('number', numbers);
@@ -92,5 +133,123 @@ function password_meter(total) {
     }
 }
 
-
 /* password */
+
+/* validate Input */
+function setError(element, message) {
+    const inputControl = element.parent();
+    const errorDisplay = inputControl.find('.error-message');
+
+    errorDisplay.text(message);
+    inputControl.addClass('error').removeClass('pass');
+}
+
+function setSuccess(element) {
+    const inputControl = element.parent();
+    const errorDisplay = inputControl.find('.error-message');
+
+    errorDisplay.text('');
+    inputControl.addClass('pass').removeClass('error');
+}
+
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function isNumber(value) {
+    return !isNaN(value) && typeof value === 'number';
+}
+
+function validateInputs() {
+    var total = 0
+    const firstValue = $('#firstname').val();
+    const lastnameValue = $('#lastname').val();
+    const dateValue = $('#birthday').val();
+    const tel = $('#tol').val();
+    const id_number = $('#id_number').val();
+    
+    const emailValue = $('#email').val();
+    const passwordValue = $('#register-password').val();
+    const password2Value = $('#password_confirm').val();
+
+    if(firstValue === '') {
+        setError($('#firstname'), 'กรุณาระบุชื่อ');
+    } else {
+        setSuccess($('#firstname'));
+        total +=1;
+    }
+
+    if(lastnameValue === '') {
+        setError($('#lastname'), 'กรุณาระบุนามสกุล');
+    } else {
+        setSuccess($('#lastname'));
+        total +=1;
+    }
+
+    if(dateValue === '') {
+        setError($('#birthday'), 'กรุณาระบุวันเกิด');
+    } else {
+        setSuccess($('#birthday'));
+        total +=1;
+    }
+
+    if(tel === '') {
+        setError($('#tol'), 'กรุณาระบุเบอร์โทร');
+    }  
+    else if (!isNumber(Number(tel))){
+        setError($('#tol'), 'กรุณากรอกเฉพาะตัวเลข');
+    } 
+    else if (tel.length < 8) {
+        setError($('#tol'), 'must be at least 10 characters');
+    }
+    else {
+        setSuccess($('#tol'));
+        total +=1;
+    }
+
+    if(id_number === '') {
+        setError($('#id_number'), 'กรุณาระบุรหัสบัตรประชาชน');
+    }  
+    else if (!isNumber(Number(id_number))){
+        setError($('#id_number'), 'กรุณากรอกเฉพาะตัวเลข');
+    }
+    else if (id_number.length < 8) {
+        setError($('#id_number'), 'must be at least 13 characters');
+    }
+    else {
+        setSuccess($('#id_number'));
+        total +=1;
+    }
+
+    if(emailValue === '') {
+        setError($('#email'), 'กรุณาระบุอีเมลล์');
+    } else if (!isValidEmail(emailValue)) {
+        setError($('#email'), 'กรุณากรอกรูปแบบ อีเมลล์ ให้ถูกต้อง');
+    } else {
+        setSuccess($('#email'));
+    }
+
+    if(passwordValue === '') {
+        setError($('#register-password'), 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError($('#register-password'), 'Password must be at least 8 characters');
+    } else {
+        setSuccess($('#register-password'));
+        total +=1;
+    }
+
+    if(password2Value === '') {
+        setError($('#password_confirm'), 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError($('#password_confirm'), "Passwords don't match");
+    } else {
+        setSuccess($('#password_confirm'));
+        total +=1;
+    }
+
+    if (total === 7){
+        return true;
+    }
+}
+/* validate Input */
--- project/views.py
+++ project/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'hotelpage.html')
 
 def register(request):
+    if request.POST:
+        print (request.POST)
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        birthday = request.POST.get('birthday')
+        tol = request.POST.get('tol')
+        id_number = request.POST.get('id_number')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(firstname)
+        print(lastname)
+        print(birthday)
+        print(tol)
+        print(id_number)
+        print(email)
+        print(password)
+        return HttpResponseRedirect(reverse('login'))
     return render(request, 'register.html')
 
 def login(request):
