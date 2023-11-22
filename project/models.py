45d976687a7fe8116b1c7ab363d515982facf300
fumizz01 <phoomfairylight@gmail.com>
2023-11-23 00:17:05 +0700

    modified model.py and add em_table.html/css, both are example files

--- project/models.py
+++ project/models.py
@@ -1,9 +1,21 @@
-""" from django.db import models
+from django.db import models
 
 # Create your models here.
 
+class Account(models.Model):
+    user_id = models.CharField(max_length=10, primary_key=True)
+    user_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, null=True, blank=True)
+    password = models.CharField(max_length=20, null=True, blank=True)
+    class Meta:
+        db_table = "account" 
+        managed = True 
+    def __str__(self):
+        return self.user_id
+
 class Customer(models.Model):
-    customer_id = models.CharField(max_length=50)
+    customer_id = models.CharField(max_length=10, primary_key=True)
+    user_id = models.ForeignKey(Account, on_delete=models.CASCADE, db_column='user_id')
     identification_number = models.IntegerField()
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=30, null=True, blank=True)
@@ -12,10 +24,10 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     class Meta:
         db_table = "customer"
-        unique_together = ("customer_id", "identification_number")
         managed = True
     def __str__(self):
-        return '{"receipt_no":"%s","item_no":"%s"}' % (self.customer_id, self.identification_number)
+        return self.customer_id 
+    
     
 class Reservation(models.Model):
     reservation_id = models.IntegerField(primary_key=True)
@@ -29,19 +41,6 @@ class Reservation(models.Model):
         managed = True
     def __str__(self):
         return self.reservation_id
-    
-class Account(models.Model):
-    user_id = models.CharField(max_length=10)
-    user_name = models.CharField(max_length=50)
-    id = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column='customer_id')
-    role = models.CharField(max_length=50, null=True, blank=True)
-    password = models.CharField(max_length=20, null=True, blank=True)
-    class Meta:
-        db_table = "account" 
-        unique_together = ("user_id", "user_name")
-        managed = True 
-    def __str__(self):
-        return '{"user_id":"%s","user_name":"%s"}' % (self.user_id, self.user_name)
 
 class Room(models.Model):
     room_number = models.IntegerField(primary_key=True)
@@ -57,17 +56,17 @@ class Room(models.Model):
         return self.room_number
     
 class Employee(models.Model):
-    employee_id = models.CharField(max_length=50)
+    employee_id = models.CharField(max_length=50, primary_key=True)
+    user_id = models.ForeignKey(Account, on_delete=models.CASCADE, db_column='user_id')
     identification_number = models.IntegerField()
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=30, null=True, blank=True)
     sex = models.CharField(max_length=30, null=True, blank=True)
-    birth_date = models.IntegerField()
+    birth_date = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=100, null=True, blank=True)
     position = models.CharField(max_length=20, null=True, blank=True)
     class Meta:
         db_table = "employee"
-        unique_together = ("employee_id", "identification_number")
         managed = True 
     def __str__(self):
-        return '{"employee_id":"%s","identification_number":"%s"}' % (self.employee_id, self.identification_number) """
+        return self.employee_id
