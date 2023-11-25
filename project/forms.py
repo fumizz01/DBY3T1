90fbdb98e58fb056b05a1b2de5700ea4cddee24a
Patnakub <thanapatthaiking@gmail.com>
2023-11-26 00:36:10 +0700

    ทำหน้า login และ register

---
+++ project/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import *
+
+
+""" class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Account
+        fields = UserCreationForm.Meta.fields """
+        
+""" class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields """
+        
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('user_code', 'role')
+
+"""class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = '__all__'"""
